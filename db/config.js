4ce16b41144bd2163a2a54f07cb2c5a794fb1a19
welldanyogia <welldanyogia01@gmail.com>
2024-08-06 19:32:50 +0700

    update config.js

--- db/config.js
+++ db/config.js
@@ -1,31 +1,31 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.owner = ['6285237859745']
-global.nomerOwner = '6285237859745'
-global.ownerName = 'Lugas Prasetiyo'
+global.owner = ['6282131670100']
+global.nomerOwner = '6282131670100'
+global.ownerName = 'Webrana Store'
 
-global.nomerBot = '6285788869024' 
-global.botName = 'Gemtech.ID'
-global.sessionName = 'session' 
+global.nomerBot = '6287886296587'
+global.botName = 'Webrana Bot'
+global.sessionName = 'session'
 
-namaStore = '' // NAMA STORE KAMU
+namaStore = 'Webrana Store' // NAMA STORE KAMU
 
 // PAYDISINI
-APIKEY_PAYDISINI = '' // API KEY
+APIKEY_PAYDISINI = 'ba2fb599c53ad7e801ec79f7915f4522' // API KEY
 
-username = '' // ISI USERNAME DIGIFLAZZ
-apiKey = ''   // ISI APIKEY PRODUCTION DIGIFLAZZ
-marginBronze = 0.03 // 3%
+username = 'mohoveg4ML6o' // ISI USERNAME DIGIFLAZZ
+apiKey = '0a0846e7-a2c4-5547-88ec-07b0449d3cee'   // ISI APIKEY PRODUCTION DIGIFLAZZ
+marginBronze = 0.05 // 3%
 marginSilver = 0.025 // 2.5%
 marginGold = 0.02 // 2%
 marginOwner = 0.03 // 3%
 paymentKamu = ``
 
 linkLOGO = './assets/logo.png'
-linkQRIS = 'https://telegra.ph/file/4138368f9f78e7b5074a7.jpg'
-linkGC = 'https://chat.whatsapp.com/HrJUa2AVUYhDsflaLItdUF'
-poster1 = 'https://i.ibb.co/rbQZRNR/BANNER-BOT.png'
+linkQRIS = 'https://telegra.ph/file/c80356bf94a3089c6aaa9.jpg'
+linkGC = 'https://chat.whatsapp.com/E480OfLCv7aCrXsUiuQuRq'
+poster1 = 'https://telegra.ph/file/f254df5359d822192ea43.jpg'
 
 // Respon Bot
 global.mess = {
@@ -41,8 +41,8 @@ global.mess = {
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(chalk.yellowBright(`Update File Terbaru ${__filename}`))
-delete require.cache[file]
-require(file)
+  fs.unwatchFile(file)
+  console.log(chalk.yellowBright(`Update File Terbaru ${__filename}`))
+  delete require.cache[file]
+  require(file)
 })
